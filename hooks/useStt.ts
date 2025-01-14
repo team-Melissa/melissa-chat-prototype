b9b9e2a618a1ae033cc19b937d2b4c3cad572c1e
NARARIA03 <hyeonseong0305@gmail.com>
2025-01-15 01:23:17 +0900

    KAN-4 Refactor: STT 관련 로직 함수로 분리

--- hooks/useStt.ts
+++ hooks/useStt.ts
@@ -1,4 +1,11 @@
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import Voice, { SpeechResultsEvent } from "@react-native-voice/voice";
 import { Chat } from "@/app.types";
 
@@ -10,50 +17,45 @@ export const useStt = (
   const isFinSpkRef = useRef<boolean>(false);
   const noSpkTimerRef = useRef<NodeJS.Timeout | null>(null);
 
+  const handleUpdateChats = useCallback(
+    (text: string) => {
+      if (isFinSpkRef.current) {
+        // 음성 인식 2초 이상 멈췄다가 다시 발생한 케이스 (새로운 user 필드를 만들어야 하는 상황)
+        isFinSpkRef.current = false;
+        setChats((prev) =>
+          prev[prev.length - 1].text === text
+            ? prev
+            : [...prev, { role: "user", text }]
+        );
+      } else {
+        // 음성 인식이 멈추기 전 발생한 케이스 (기존 마지막 user 필드의 텍스트만 업데이트 해야 하는 상황)
+        setChats((prev) => [...prev.slice(0, -1), { role: "user", text }]);
+      }
+    },
+    [setChats]
+  );
+
+  const handleStopStt = () => {
+    // 기존에 timeout이 등록되어 있으면 제거 후 재등록
+    if (noSpkTimerRef.current) clearTimeout(noSpkTimerRef.current);
+
+    noSpkTimerRef.current = setTimeout(async () => {
+      console.log("STT 일시 정지!");
+      Voice.cancel().then(() => Voice.start("ko-kr"));
+      isFinSpkRef.current = true;
+    }, 2000);
+  };
+
   useEffect(() => {
     if (isSpkMode) {
       console.log("음성 인식 모듈 마운트");
-
       Voice.onSpeechResults = (e: SpeechResultsEvent) => {
         if (e.value && e.value.length > 0) {
-          const text = e.value[0];
-          // 말을 해서 음성 인식 이벤트가 발생하면
-          // > 기존 timeout 제거
-          // > 새 timeout 생성
-          // > recognizedTxt 업데이트 수행 하도록 이벤트 핸들러 구현
-
-          // 음성 인식 멈췄다가 다시 발생하면 isFinSpk를 false로 업데이트
-          // 음성 인식 결과 실시간으로 chats state에 반영
-          if (isFinSpkRef.current) {
-            isFinSpkRef.current = false;
-            setChats((prev) =>
-              prev[prev.length - 1].text === text
-                ? prev
-                : [...prev, { role: "user", text }]
-            );
-          } else {
-            setChats((prev) => {
-              const lastObj = prev[prev.length - 1];
-              prev.pop();
-              lastObj.text = text;
-              return [...prev, lastObj];
-            });
-          }
-
-          // 기존에 타임아웃이 등록되어 있으면 제거 후 다시 등록
-          if (noSpkTimerRef.current) clearTimeout(noSpkTimerRef.current);
-
-          noSpkTimerRef.current = setTimeout(async () => {
-            console.log("STT 일시 정지!");
-            Voice.cancel().then(() => Voice.start("ko-kr"));
-            isFinSpkRef.current = true;
-          }, 3000);
-
-          // 음성인식 결과 따로 저장
-          setRecognizedTxt(text);
+          handleUpdateChats(e.value[0]); // 채팅에 사용자 음성인식 적용
+          handleStopStt(); // stt 종료 콜백 관리
+          setRecognizedTxt(e.value[0]);
         }
       };
-      // 음성 인식 활성화
       Voice.start("ko-kr");
     } else {
       Voice.destroy().then(Voice.removeAllListeners);
@@ -62,7 +64,7 @@ export const useStt = (
     return () => {
       Voice.destroy().then(Voice.removeAllListeners);
     };
-  }, [isSpkMode, setChats]);
+  }, [isSpkMode, handleUpdateChats]);
 
   useEffect(() => {
     console.log(recognizedTxt);
